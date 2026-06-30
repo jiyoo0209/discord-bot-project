e6d89b0f2bee88d5f75193804e057a90af3e5b2b
jiyoo0209 <127077577+jiyoo0209@users.noreply.github.com>
2026-06-30 16:59:43 +0900

    [user] register_user 추가

--- user/user.py
+++ user/user.py
@@ -1,1 +1,19 @@
-.
+'''
+* 길드원 추가/삭제
+* author LJY
+* date   2026.06.30
+'''
+import discord
+from discord.ext import commands
+from sheet.sheet import add_user
+
+def setup_user_commands(bot):
+    @bot.tree.command(name='가입', description='새로운 길드원 추가')
+    async def register_user(interaction: discord.Interaction, user_name: str):
+        try:
+            success, message = add_user(user_name)
+
+            await interaction.response.send_message(message, ephemeral=True)
+
+        except Exception as e:
+            await interaction.response.send_message(f'오류: {e}', ephemeral=True)
