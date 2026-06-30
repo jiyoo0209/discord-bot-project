7fe9dc7256b223ec863de8a66fd0212c8b909fd2
jiyoo0209 <127077577+jiyoo0209@users.noreply.github.com>
2026-06-30 17:08:11 +0900

    [user] Cogs 방식으로 수정

--- user/user.py
+++ user/user.py
@@ -7,8 +7,11 @@ import discord
 from discord.ext import commands
 from sheet.sheet import add_user
 
-def setup_user_commands(bot):
-    @bot.tree.command(name='가입', description='새로운 길드원 추가')
+class UserCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.tree.command(name='가입', description='새로운 길드원 추가')
     async def register_user(interaction: discord.Interaction, user_name: str):
         try:
             success, message = add_user(user_name)
@@ -17,3 +20,6 @@ def setup_user_commands(bot):
 
         except Exception as e:
             await interaction.response.send_message(f'오류: {e}', ephemeral=True)
+
+async def setup(bot):
+    await bot.add_cog(UserCog(bot))
