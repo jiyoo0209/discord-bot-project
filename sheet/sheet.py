d6cfb23a1f4d02aaf86febef17250c3c54da1f23
jiyoo0209 <127077577+jiyoo0209@users.noreply.github.com>
2026-06-30 11:20:22 +0900

    get_worksheet 함수 추가

--- sheet/sheet.py
+++ sheet/sheet.py
@@ -15,3 +15,16 @@ load_dotenv()
 def get_sheet():
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
     creds_dict = json.loads(os.getenv('GOOGLE_CREDENTIALS'))
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+    client = gspread.authorize(creds)
+    spreadsheet = client.open_by_key(os.getenv('SPREADSHEET_ID'))
+    return spreadsheet
+
+# 워크시트 가져오기
+def get_worksheet(sheet_name):
+    spreadsheet = get_sheet()
+    try:
+        worksheet = spreadsheet.worksheet(sheet_name)
+    except:
+        worksheet = spreadsheet.add_worksheet(title=sheet_name, row=500, cols=10)
+    return worksheet
